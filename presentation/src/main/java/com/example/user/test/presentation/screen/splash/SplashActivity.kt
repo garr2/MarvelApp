62dedd2051596993717bee6a74530b24a7e8a1cf
garr2 <funins@gmail.com>
2019-01-04 16:33:12 +0300

    Add splash activity.

---
+++ presentation/src/main/java/com/example/user/test/presentation/screen/splash/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.example.user.test.presentation.screen.splash
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.user.test.R
+import com.example.user.test.presentation.screen.man.ManActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        startActivity(Intent(this, ManActivity::class.java))
+
+    }
+
+}
