02513b0ba32dacd6c02efcf191a0190b2fc17a77
garr2 <funins@gmail.com>
2019-01-08 12:11:57 +0300

    Add splash activity.

--- presentation/src/main/java/com/example/user/test/presentation/screen/splash/SplashActivity.kt
+++ presentation/src/main/java/com/example/user/test/presentation/screen/splash/SplashActivity.kt
@@ -11,7 +11,7 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        startActivity(Intent(this, ManActivity::class.java))
+        changeActivity()
 
     }
 
